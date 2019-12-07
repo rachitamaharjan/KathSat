0ae02006ab89af9363bf8c71f5a2a1a8e4ce9f46
smilerac <smilerac15@gmail.com>
2019-12-08 02:34:52 +0545

    accelerometer codes modularized

--- CompleteCodeCansat/CompleteCodeCansat.ino
+++ CompleteCodeCansat/CompleteCodeCansat.ino
@@ -1,9 +1,13 @@
+#include "accelerometer.h"
+
+
 void setup() {
   // put your setup code here, to run once:
+ Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+accelerometerPrint();
 }
